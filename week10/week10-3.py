3da295150587a270182873de8706d4911ee16e9d
wesleycoding0226 <chenwesley531@gmail.com>
2026-04-30 15:52:43 +0800

    week10

---
+++ week10/week10-3.py
@@ -0,0 +1,19 @@
+#week10-3.py
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        def helper(root, big):
+            if root==None: return 0
+            ans = 0
+            if root.val >= big:
+                ans += 1
+                big = root.val
+            ans += helper(root.left, big)
+            ans += helper(root.right, big)
+            return ans
+        return helper(root, root.val)
---
+++ week10/week10-6.py
@@ -0,0 +1,19 @@
+#week10-6.py
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def longestZigZag(self, root: Optional[TreeNode]) -> int:
+        self.ans = 0
+        def helper(root):
+            if root==None: return 0, 0
+
+            Lans1, Lans2 = helper(root.left)
+            Rans1, Rans2 = helper(root.right)
+            self.ans = max(self.ans, Lans2 + 1, Rans1 + 1)
+            return Lans2 + 1, Rans1 + 1
+        helper(root)
+        return self.ans - 1
